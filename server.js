7a0c756c4f3f556d9c00727bb39ba822c449b381
T4nGe4nT <nichholcomb@gmail.com>
2024-07-02 16:51:29 -0400

    Made it work

--- server.js
+++ server.js
@@ -2,23 +2,33 @@ const http = require('http');
 
 
 const server = http.createServer((req, res) => {
-    if (req.method === "/") {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('<h1>Welcome</h1>');
-    } else if (req.method === '/api') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ message: "API is running" }));
-    } else { 
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Does not exist');
-    }
+    if (req.method === "GET") 
+;
+
+    switch (req.url) {
+        case '/':
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('<h1>Welcome</h1>')
+            break;
+
+        case '/api':
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify('Greetings from your API'));
+            break;
+
+        default:
+            res.statusCode = 404;
+            res.end('<h1>Does Not Exist</h1>');
+            break;
+                
+  
+};
 });
 
 const PORT = 3000;
 server.listen(PORT, () => {
 
     console.log(`Server is running at http://localhost:${PORT}/`);
-})
+});
